d1987189b6e1bc3cd604a23c0ecfd29f0f1e89cf
erun94510 <erun9414@icloud.com>
2022-12-02 10:19:52 +0900

    add

---
+++ githubTest/githubTest/githubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubTestApp.swift
+//  githubTest
+//
+//  Created by Donghoon Bae on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct githubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
