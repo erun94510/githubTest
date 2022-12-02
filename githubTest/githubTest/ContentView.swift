6262336100c652fc53861c8bc7f6973d5aefe98e
erun94510 <erun9414@icloud.com>
2022-12-02 10:44:03 +0900

    Revert "commit"
    This reverts commit e53d9ca1d725401d4b2a483c9eb5b9521eb4a6f9.

--- githubTest/githubTest/ContentView.swift
+++ githubTest/githubTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!123123123123fdsafdsafdsa")
+            Text("Hello, world!123123123123")
         }
         .padding()
     }
